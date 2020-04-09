7ec5eba7a929dacb8f178c5b88f5791fba6067a5
Michael Olbrich <m.olbrich@pengutronix.de>
2020-04-09 09:37:30 +0200

    ptxd_make_nfsd_exec: sync with upstream
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- configs/platform-v7a/scripts/lib/ptxd_make_nfsd.sh
+++ configs/platform-v7a/scripts/lib/ptxd_make_nfsd.sh
@@ -8,6 +8,7 @@
 
 ptxd_make_nfsd_exec() {
     local port
+    local client_specifications
     local root="/$(basename "${ptx_nfsroot}")"
     local base="$(dirname "${ptx_nfsroot}")"
 
@@ -15,13 +16,18 @@ ptxd_make_nfsd_exec() {
 	port=2049
     fi
 
+    if ! client_specifications="$(ptxd_get_kconfig "${PTXDIST_BOARDSETUP}" "PTXCONF_BOARDSETUP_NFSROOT_CLIENT_SPECIFICATIONS")"; then
+	client_specifications="(rw,no_root_squash)"
+    fi
+
     echo
     echo "Mount rootfs with nfsroot=${root},v3,tcp,port=${port},mountport=${port}"
     echo
 
     #insecure for qemu with -net user
-    echo "/ (rw,no_root_squash,insecure)" > "${PTXDIST_TEMPDIR}/exports" &&
-    UNFS_BASE="${base}" unfsd -e "${PTXDIST_TEMPDIR}/exports" -n ${port} -m ${port} -p -d
+    client_specifications="${client_specifications},insecure"
+    echo "/ ${client_specifications}" > "${PTXDIST_TEMPDIR}/exports" &&
+    UNFS_BASE="${base}" unfsd -e "${PTXDIST_TEMPDIR}/exports" -n ${port} -m ${port} -p -d "${@}"
 }
 export -f ptxd_make_nfsd_exec
 

jluebbe/DistroKit

commit 1db122202276b9f343ebdb386df961b2a8cb14a5

platform-v7a: rpi2: add board support
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

configs/platform-v7a/rules/image-rpi2.make
@@ -1,0 +1,49 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2017 by Sascha Hauer <s.hauer@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+IMAGE_PACKAGES-$(PTXCONF_IMAGE_RPI2) += image-rpi2
+
+#
+# Paths and names
+#
+IMAGE_RPI2		:= image-rpi2
+IMAGE_RPI2_DIR	:= $(BUILDDIR)/$(IMAGE_RPI2)
+IMAGE_RPI2_IMAGE	:= $(IMAGEDIR)/rpi2.hdimg
+IMAGE_RPI2_FILES	:= $(IMAGEDIR)/root.tgz
+IMAGE_RPI2_CONFIG	:= rpi2.config
+IMAGE_RPI2_DATA    := \
+        $(wildcard $(PTXDIST_PLATFORMCONFIGDIR)/rpi-firmware/*.bin) \
+        $(wildcard $(PTXDIST_PLATFORMCONFIGDIR)/rpi-firmware/*.elf) \
+        $(wildcard $(PTXDIST_PLATFORMCONFIGDIR)/rpi-firmware/*.dat) \
+        $(wildcard $(PTXDIST_PLATFORMCONFIGDIR)/rpi-firmware/config.txt)
+
+# ----------------------------------------------------------------------------
+# Image
+# ----------------------------------------------------------------------------
+
+define squote_and_comma
+$(subst $(ptx/def/space),$(comma) ,$(addsuffix $(ptx/def/squote),$(addprefix $(ptx/def/squote),$(1))))
+endef
+
+IMAGE_RPI2_ENV := \
+        FIRMWARE_RPI2="$(call squote_and_comma,$(IMAGE_RPI2_DATA))"
+
+$(IMAGE_RPI2_IMAGE):
+	@$(call targetinfo)
+	@GPU_MEM=$(PTXCONF_IMAGE_RPI2_GPU_MEM) \
+		ptxd_replace_magic "$(PTXDIST_PLATFORMCONFIGDIR)/rpi-firmware/config.txt" > \
+		"$(PTXDIST_TEMPDIR)/config.txt"
+	@$(call image/genimage, IMAGE_RPI2)
+	@$(call finish)
+
+# vim: syntax=make

configs/platform-v7a/config/images/rpi2.config
@@ -1,0 +1,29 @@
+image rpi2-boot.vfat {
+	vfat {
+		files = { @FIRMWARE_RPI2@ }
+		file barebox.bin { image = barebox-raspberry-pi-2.img-rpi2 }
+	}
+	size = 32M
+}
+
+image @IMAGE@ {
+	hdimage {
+		align = 1M
+		disk-signature = 0xbedf7893
+	}
+	partition boot {
+		image = rpi2-boot.vfat
+		size = 32M
+		partition-type = 0xc
+		bootable = true
+	}
+	partition root-A {
+		image = root.ext2
+		partition-type = 0x83
+		size = 512M
+	}
+	partition data {
+		partition-type = 0x83
+		size = 512M
+	}
+}

configs/platform-v7a/projectroot/loader/entries/rpi2.conf
@@ -1,5 +1,6 @@
 title		Raspberry Pi 2 - Pengutronix-DistroKit
 version		4.6
-options		loglevel=5 systemd.log_level=warning systemd.show_status=auto root=/dev/mmcblk0p2 rootfstype=ext4 rootwait rw
+options		loglevel=5 systemd.log_level=warning systemd.show_status=auto rootwait rw
 linux		/boot/zImage
 devicetree	/boot/bcm2836-rpi-2-b.dtb
+linux-appendroot true

commit c22b77340be7a0a0b7322776e6d579ef516ba5ad

image-rpi2, image-rpi3: remove no-op ptxd_replace_magic calls
The new 'ptxdist lint' feature complained about these lines:

    Undefined symbol PTXCONF_IMAGE_RPI2_GPU_MEM used in 'next/configs/platform-v7a/rules/image-rpi2.make'.
    Undefined symbol PTXCONF_IMAGE_RPI3_GPU_MEM used in 'next/configs/platform-v7a/rules/image-rpi3.make'.

Indeed, these complaints were caused by multiple issues, probably going
back to copy-and-paste from platform-rpi:

* IMAGE_RPI2_GPU_MEM and IMAGE_RPI3_GPU_MEM were never defined in any
  kconfig menu since these lines were added (but a similar kconfig item
  exists in configs/platform-rpi)
* configs/platform-v7a/rpi-firmware/config.txt didn't have any magic
  replacement markers anymore since commit 98626f8441add596486b
* the "replaced" config.txt was never used by anything after writing it
  to PTXDIST_TEMPDIR (which is the case in configs/platform-rpi)

Remove the call completely.

Fixes: 1db122202276b9f343eb (2017-06-16, "platform-v7a: rpi2: add board support")
Fixes: 98626f8441add596486b (2018-11-29, "add RPI3 Support")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>

configs/platform-v7a/rules/image-rpi2.make
@@ -40,9 +40,6 @@ IMAGE_RPI2_ENV := \
 
 $(IMAGE_RPI2_IMAGE):
 	@$(call targetinfo)
-	@GPU_MEM=$(PTXCONF_IMAGE_RPI2_GPU_MEM) \
-		ptxd_replace_magic "$(IMAGE_RPI2_DATA_DIR)/config.txt" > \
-		"$(PTXDIST_TEMPDIR)/config.txt"
 	@$(call image/genimage, IMAGE_RPI2)
 	@$(call finish)
 

configs/platform-v7a/rules/image-rpi3.make
@@ -40,9 +40,6 @@ IMAGE_RPI3_ENV := \
 
 $(IMAGE_RPI3_IMAGE):
 	@$(call targetinfo)
-	@GPU_MEM=$(PTXCONF_IMAGE_RPI3_GPU_MEM) \
-		ptxd_replace_magic "$(IMAGE_RPI3_DATA_DIR)/config.txt" > \
-		"$(PTXDIST_TEMPDIR)/config.txt"
 	@$(call image/genimage, IMAGE_RPI3)
 	@$(call finish)
 
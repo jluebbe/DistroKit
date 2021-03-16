jluebbe/DistroKit

commit 2e2c4377dc083855e07b9e40cdfc3127073c3d46

i.MX8M: add firmware-imx 8.8
Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

configs/platform-v8a/rules/firmware-imx.make
@@ -1,0 +1,85 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2016 by Philipp Zabel <p.zabel@pengutronix.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_FIRMWARE_IMX) += firmware-imx
+
+#
+# Paths and names
+#
+FIRMWARE_IMX_VERSION	:= 8.8
+FIRMWARE_IMX_MD5	:= eabb27d28bba375a9f14d6306c07af5f
+FIRMWARE_IMX_SKIP	:= 38918
+FIRMWARE_IMX		:= firmware-imx-$(FIRMWARE_IMX_VERSION)
+FIRMWARE_IMX_SUFFIX	:= bin
+FIRMWARE_IMX_URL	:= http://www.nxp.com/lgfiles/NMG/MAD/YOCTO/$(FIRMWARE_IMX).$(FIRMWARE_IMX_SUFFIX)
+FIRMWARE_IMX_LICENSE	:= NXP-Software-License-Agreement
+FIRMWARE_IMX_LICENSE_FILES := \
+	file://$(FIRMWARE_IMX_PKGDIR)/COPYING;md5=228c72f2a91452b8a03c4cab30f30ef9
+FIRMWARE_IMX_SOURCE	:= $(SRCDIR)/$(FIRMWARE_IMX).$(FIRMWARE_IMX_SUFFIX)
+
+# ----------------------------------------------------------------------------
+# Extract
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/firmware-imx.extract:
+	@$(call targetinfo)
+	@dd if=$(FIRMWARE_IMX_SOURCE) bs=$(FIRMWARE_IMX_SKIP) skip=1 | tar xj -C $(PKGDIR)
+	@$(call touch)
+
+# ----------------------------------------------------------------------------
+# Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/firmware-imx.install:
+	@$(call targetinfo)
+
+ifdef PTXCONF_FIRMWARE_IMX_BOOTIMAGE_IMX8
+	@$(foreach f, lpddr4_pmu_train_1d_dmem.bin lpddr4_pmu_train_1d_imem.bin \
+	              lpddr4_pmu_train_2d_dmem.bin lpddr4_pmu_train_2d_imem.bin, \
+		install -v -D -m644 $(FIRMWARE_IMX_PKGDIR)/firmware/ddr/synopsys/$(f) \
+		$(PTXCONF_SYSROOT_TARGET)/firmware/imx/$(f);)
+
+	@$(foreach f, signed_dp_imx8m.bin signed_hdmi_imx8m.bin, \
+		install -v -D -m644 $(FIRMWARE_IMX_PKGDIR)/firmware/hdmi/cadence/$(f) \
+		$(PTXCONF_SYSROOT_TARGET)/firmware/imx/$(f);)
+endif
+
+	@$(call touch)
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+FIRMWARE_IMX_INSTALL-y					:=
+FIRMWARE_IMX_INSTALL-$(PTXCONF_FIRMWARE_IMX_VPU_IMX27)	+= vpu_fw_imx27_TO2.bin
+FIRMWARE_IMX_INSTALL-$(PTXCONF_FIRMWARE_IMX_VPU_IMX6Q)	+= vpu_fw_imx6q.bin
+FIRMWARE_IMX_INSTALL-$(PTXCONF_FIRMWARE_IMX_VPU_IMX6DL)	+= vpu_fw_imx6d.bin
+FIRMWARE_IMX_INSTALL-$(PTXCONF_FIRMWARE_IMX_VPU_IMX53)	+= vpu_fw_imx53.bin
+FIRMWARE_IMX_INSTALL-$(PTXCONF_FIRMWARE_IMX_VPU_IMX51)	+= vpu_fw_imx51.bin
+
+$(STATEDIR)/firmware-imx.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, firmware-imx)
+	@$(call install_fixup, firmware-imx,PRIORITY,optional)
+	@$(call install_fixup, firmware-imx,SECTION,base)
+	@$(call install_fixup, firmware-imx,AUTHOR,"Philipp Zabel <p.zabel@pengutronix.de>")
+	@$(call install_fixup, firmware-imx,DESCRIPTION,missing)
+	@for f in $(FIRMWARE_IMX_INSTALL-y); do \
+		$(call install_copy, firmware-imx, 0, 0, 0644, \
+		$(FIRMWARE_IMX_PKGDIR)/firmware/vpu/$$f, /lib/firmware/$$f); \
+	done
+	@$(call install_finish, firmware-imx)
+
+	@$(call touch)
+
+# vim: syntax=make

configs/platform-v8a/rules/firmware-imx.in
@@ -1,0 +1,49 @@
+## SECTION=hosttools_platform
+# This should be in a firmware section, once we added that.
+
+menuconfig FIRMWARE_IMX
+	tristate
+	prompt "firmware-imx                  "
+	help
+	  Install CODA VPU firmware files from the firmware-imx package.
+
+if FIRMWARE_IMX
+
+config FIRMWARE_IMX_VPU_IMX27
+	tristate
+	prompt "CodaDx6 firmware for i.MX27"
+	help
+	  Install the CODA7541 VPU firmware for i.MX27.
+
+config FIRMWARE_IMX_VPU_IMX51
+	tristate
+	prompt "CodaHx4 firmware for i.MX51"
+	help
+	  Install the CodaHx4 VPU firmware for i.MX51.
+
+config FIRMWARE_IMX_VPU_IMX53
+	tristate
+	prompt "CODA7541 firmware for i.MX53"
+	help
+	  Install the CODA7541 VPU firmware for i.MX53.
+
+config FIRMWARE_IMX_VPU_IMX6DL
+	tristate
+	prompt "CODA960 firmware for i.MX6DL/S"
+	help
+	  Install the CODA960 VPU firmware for i.MX6DL and i.MX6S.
+
+config FIRMWARE_IMX_VPU_IMX6Q
+	tristate
+	prompt "CODA960 firmware for i.MX6Q/D"
+	help
+	  Install the CODA960 VPU firmware for i.MX6Q and i.MX6D.
+
+config FIRMWARE_IMX_BOOTIMAGE_IMX8
+	bool
+	prompt "Install i.MX8 DDR PHY and HDMI/eDP controller firmware"
+	help
+	  Install the Synopsis DDR PHY controller and Cadence HDMI TX
+	  firmware used to build a boot image.
+
+endif

commit 84f0d0abcd1b23c7e347bbe93a1c34094245bb9e

v8a/firmware-imx: Ensure PKGDIR exists
This fixes:

tar: DistroKit/platform-v8a/packages: Cannot open: No such file or directory
tar: Error is not recoverable: exiting now
make: *** [DistroKit/configs/platform-v8a/rules/firmware-imx.make:36: DistroKit/platform-v8a/state/firmware-imx.extract] Error 2

e.g. when running

	ptxdist extract firmware-imx

in an unbuilt tree.

Fixes: 2e2c4377dc08 ("i.MX8M: add firmware-imx 8.8")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

configs/platform-v8a/rules/firmware-imx.make
@@ -33,6 +33,7 @@ FIRMWARE_IMX_SOURCE	:= $(SRCDIR)/$(FIRMWARE_IMX).$(FIRMWARE_IMX_SUFFIX)
 
 $(STATEDIR)/firmware-imx.extract:
 	@$(call targetinfo)
+	@mkdir -p "$(PKGDIR)"
 	@dd if=$(FIRMWARE_IMX_SOURCE) bs=$(FIRMWARE_IMX_SKIP) skip=1 | tar xj -C $(PKGDIR)
 	@$(call touch)
 
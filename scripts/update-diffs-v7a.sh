908158902f9c2ddd9c71054ba8eb071e6e202bcf
Roland Hieber <rhi@pengutronix.de>
2022-09-05 13:07:09 +0200

    scripts: update-diffs-v7a.sh: fix ptxdist path
    Use the same detection logic as in update-diffs-mips.sh, in case a
    different PTXdist version is to be used.
    
    Signed-off-by: Roland Hieber <rhi@pengutronix.de>
    Link: https://lore.pengutronix.de/20220905110709.1832516-6-rhi@pengutronix.de
    Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>

--- scripts/update-diffs-v7a.sh
+++ scripts/update-diffs-v7a.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+if [ -z "$PTXDIST" ]; then
+	PTXDIST=ptxdist
+else
+	tput setaf 3 # yellow
+	echo Note: using PTXDIST=$PTXDIST
+	tput sgr 0   # back to normal
+fi
+
 BAREBOXES="barebox-common barebox-am335x barebox-mx6 barebox-rpi2 barebox-vexpress"
 for pkg in $BAREBOXES; do
-	ptxdist/bin/ptxdist oldconfig $pkg
+	$PTXDIST --platformconfig="configs/platform-v7a/platformconfig" oldconfig $pkg
 done

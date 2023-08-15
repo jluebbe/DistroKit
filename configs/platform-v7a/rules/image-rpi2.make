jluebbe/DistroKit

commit 4dbcbb49876993db8a163b996080c86ff8b223f3

v7a: rpi2: move rpi-firmware directory to top-level workspace
The Raspberry Pi 3 we already support can be operated in 64-bit mode as
well as the not-yet-supported Raspberry Pi 4. Instead of duplicating the
Raspberry Pi Firmware for both platforms, move it to a central location.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
Link: https://lore.pengutronix.de/20220930162159.814389-4-a.fatoum@pengutronix.de
Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>

configs/platform-v7a/rules/image-rpi2.make
@@ -19,7 +19,7 @@ IMAGE_RPI2_DIR	:= $(BUILDDIR)/$(IMAGE_RPI2)
 IMAGE_RPI2_IMAGE	:= $(IMAGEDIR)/rpi.hdimg
 IMAGE_RPI2_FILES	:= $(IMAGEDIR)/root.tgz
 IMAGE_RPI2_CONFIG	:= rpi2.config
-IMAGE_RPI2_DATA_DIR	:= $(call ptx/in-platformconfigdir, rpi-firmware)
+IMAGE_RPI2_DATA_DIR	:= $(PTXDIST_WORKSPACE)/rpi-firmware
 IMAGE_RPI2_DATA		:= \
 	$(wildcard $(IMAGE_RPI2_DATA_DIR)/*.bin) \
 	$(wildcard $(IMAGE_RPI2_DATA_DIR)/*.elf) \

commit a96a739eee17f6994c38f3621cc25980e229e507

v7a: rpi2: Restore finding rpi-firmware when used as base layer
When DistroKit was used as a base layer, the firmware was not in
'$(PTXDIST_WORKSPACE)/rpi-firmware' but in
'$(PTXDIST_WORKSPACE)/base/rpi-firmware' or
'$(PTXDIST_WORKSPACE)/base/base/rpi-firmware' … so we just need to look
for it in all layers.  Otherwise no firmware is put to the FAT partition
of the SD card image and the device does not boot at all.

Fixes: 4dbcbb498769 ("v7a: rpi2: move rpi-firmware directory to top-level workspace")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Reviewed-by: Michael Olbrich <m.olbrich@pengutronix.de>
Link: https://lore.barebox.org/20230630120847.3271567-1-ada@thorsis.com
Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>

configs/platform-v7a/rules/image-rpi2.make
@@ -19,7 +19,7 @@ IMAGE_RPI2_DIR	:= $(BUILDDIR)/$(IMAGE_RPI2)
 IMAGE_RPI2_IMAGE	:= $(IMAGEDIR)/rpi.hdimg
 IMAGE_RPI2_FILES	:= $(IMAGEDIR)/root.tgz
 IMAGE_RPI2_CONFIG	:= rpi2.config
-IMAGE_RPI2_DATA_DIR	:= $(PTXDIST_WORKSPACE)/rpi-firmware
+IMAGE_RPI2_DATA_DIR	:= $(call ptx/in-path, PTXDIST_PATH, rpi-firmware)
 IMAGE_RPI2_DATA		:= \
 	$(wildcard $(IMAGE_RPI2_DATA_DIR)/*.bin) \
 	$(wildcard $(IMAGE_RPI2_DATA_DIR)/*.elf) \